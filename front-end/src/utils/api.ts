74d48885dcf9c5664d750214ce26abf2b27314c0
boytur <mask30254@gmail.com>
2024-07-21 20:25:13 +0700

    ✨ feat[api.ts]: addd axios instance

---
+++ front-end/src/utils/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL, 
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
