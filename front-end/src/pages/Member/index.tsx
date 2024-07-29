45c980b62e3a11128095509658458ef847558355
warot020210 <65160235@go.buu.ac.th>
2024-07-29 15:32:49 +0700

    ✨feat[Member/index.tsx]: add feat about individual work placement by pulling data from api doc

--- front-end/src/pages/Member/index.tsx
+++ front-end/src/pages/Member/index.tsx
@@ -1,6 +1,59 @@
-import React from "react";
+import { useAuthContext } from "@/contexts/authContext";
+import { UserTaskCount } from "@/interfaces/User.interface";
+import { api } from "@/utils/api";
+import React, { useEffect, useState } from "react";
 
 const Member: React.FC = () => {
-  return <div>Member Page</div>;
+  const { authState } = useAuthContext();
+  const [users, setUsers] = useState<UserTaskCount[]>([]);
+  const fetchUser = async () => {
+    try {
+      const respon = await api.get("/api/user/users");
+      console.log(respon.data);
+      setUsers(respon.data.users);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  useEffect(() => {
+    // if(!authState.isLoggedin){
+    //   return;
+    // }
+    fetchUser();
+  }, [])
+
+
+  return (
+    <div className="w-full ">
+      Member Page
+      <table className="w-full border-radius">
+        <thead className="table-thead  ">
+          <tr >
+            <th scope="col" className="table-th">ลำดับ</th>
+            <th scope="col" className="table-th">รูป</th>
+            <th scope="col" className="table-th">ชื่อ</th>
+            <th scope="col" className="table-th">นามสกุล</th>
+            <th scope="col" className="table-th">จำนวนงาน</th>
+
+          </tr>
+        </thead>
+        <tbody className="table-tbody">
+          {users && users.map((user: UserTaskCount, index: number) => (
+            <tr key={user.discord_id} className="table-tr">
+              <td className="table-td">{index + 1}</td>
+              <td className="" >
+                <img className=" object-cover w-[3rem] h-[3rem] rounded-full" src={user.avatar} alt="" /></td>
+              <td className="table-td">{user.user_fname}</td>
+              <td className="table-td">{user.user_lname}</td>
+              <td className="table-td">{user.task_count}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+
+  )
 };
 export default Member;
