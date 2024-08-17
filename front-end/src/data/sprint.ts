b72897009d37992f243c624974d21d5965122a0c
boytur <104257779+boytur@users.noreply.github.com>
2024-08-18 00:16:04 +0700

    ♻️refactor[Retrospective/index.tsx]: delete auto fetch wenwell in sprint and added fetch wenwell by sprint instead

---
+++ front-end/src/data/sprint.ts
@@ -0,0 +1,52 @@
+export interface Sprint {
+  sprint_name: string;
+  sprint_start: string;
+  sprint_end: string;
+}
+export const sprints: Sprint[] = [
+  {
+    sprint_name: "1/1",
+    sprint_start: "2024-07-02",
+    sprint_end: "2024-07-08",
+  },
+  {
+    sprint_name: "1/2",
+    sprint_start: "2024-07-09",
+    sprint_end: "2024-07-15",
+  },
+  {
+    sprint_name: "1/3",
+    sprint_start: "2024-07-16",
+    sprint_end: "2024-07-22",
+  },
+  {
+    sprint_name: "1/4",
+    sprint_start: "2024-07-23",
+    sprint_end: "2024-07-29",
+  },
+  {
+    sprint_name: "1/5",
+    sprint_start: "2024-07-30",
+    sprint_end: "2024-08-05",
+  },
+  {
+    sprint_name: "1/6",
+    sprint_start: "2024-08-06",
+    sprint_end: "2024-08-12",
+  },
+  {
+    sprint_name: "1/7",
+    sprint_start: "2024-08-13",
+    sprint_end: "2024-08-19",
+  },
+  {
+    sprint_name: "1/8",
+    sprint_start: "2024-08-20",
+    sprint_end: "2024-08-26",
+  },
+  {
+    sprint_name: "1/9",
+    sprint_start: "2024-08-27",
+    sprint_end: "2024-09-02",
+  },
+];
